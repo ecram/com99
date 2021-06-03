7e89cb32fd08afe1ccb8bad55d5f7cf16097890e
ecram <marcelpalmas@gmail.com>
2021-06-02 21:36:20 -0400

    Actualizado a las 21:36

---
+++ ejemplo_python.py
@@ -0,0 +1,11 @@
+a = 1.1 # Esta variable es de tipo decimal
+b = 10 	# Esta variable es de tipo enterq
+print("La suma de ",a," + ",b," = ",a+b)
+
+for i in range(1,10):
+	print(i)
+
+while condeción:
+
+class persona():
+	
